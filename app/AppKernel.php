7bb1e72e7852e13adea3a6e0d6623e6595fc0a80
KULDIP PIPALIYA <kuldipem@gmail.com>
2014-05-03 16:09:36 +0530

    Front Package and Subpackage AssetsBundle generated

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MyBlog\CoreBundle\MyBlogCoreBundle(),
+            new MyBlog\Front\AssetsBundle\MyBlogFrontAssetsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
